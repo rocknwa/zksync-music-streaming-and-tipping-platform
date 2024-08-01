bae840efc564117bbf6c1cd6fb350357249ba379
MunimIftikhar <129931419+munimmetaschool@users.noreply.github.com>
2024-08-01 09:41:49 +0500

    Update Deploy.ts

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -2,11 +2,13 @@ import { deployContract } from "./utils";
 
 
 export default async function () {
-  const Paymaster = "GeneralPaymaster";
-  const PaymasterArguments = [];
-  await deployContract(Paymaster, PaymasterArguments);
+    // Deploying the GeneralPaymaster contract
+    const Paymaster = "GeneralPaymaster";
+    const PaymasterArguments = [];
+    await deployContract(Paymaster, PaymasterArguments);
 
-  const contractArtifactName = "zkTune";
-  const dAppArguments = [];
-  await deployContract(contractArtifactName, dAppArguments);
+    // Deploying the zkTune contract
+    const contractArtifactName = "zkTune";
+    const dAppArguments = [];
+    await deployContract(contractArtifactName, dAppArguments);
 }
