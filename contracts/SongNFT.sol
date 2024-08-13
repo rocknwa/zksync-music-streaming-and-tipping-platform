fb40dfb351ca1fa342f4c86a0cf3a4a9595b4954
MunimIftikhar <129931419+munimmetaschool@users.noreply.github.com>
2024-08-13 11:24:58 +0500

    Update Code

--- contracts/SongNFT.sol
+++ contracts/SongNFT.sol
@@ -8,7 +8,9 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 // Provides safe mathematical operations to prevent overflow and underflow errors.
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
+// SongNFT contract declaration
 contract SongNFT is ERC721URIStorage, Ownable {
+    // Importing SafeMath library
     using SafeMath for uint256;
 
     // Tracks the current token ID
@@ -24,12 +26,16 @@ contract SongNFT is ERC721URIStorage, Ownable {
     // URI of the cover image
     string public coverURI;
 
-    // Defining the struct NFTInfo to store comprehensive information about the NFT
+    // Defining the struct NFTInfo to store comprehensive information of the NFT
+    // Implement the fields same as state variables
     struct NFTInfo {
-        // Implement the fields same as state variables
+        uint256 currentTokenId,
+        uint256 public nftPrice,
+        // YOUR_CODE_GOES_HERE | ASSIGNMENT #1
     } 
 
-    uint256 public constant ROYALTY_PERCENTAGE = 30; // 30% royalty on NFT minting 
+    // Declaring a constant representing the royalty percentage (30%) on NFT minting
+    uint256 public constant ROYALTY_PERCENTAGE = 30;
 
     // Triggered when a new NFT is minted
     event NFTMinted(uint256 indexed tokenId, address indexed buyer, uint256 price);
@@ -38,21 +44,28 @@ contract SongNFT is ERC721URIStorage, Ownable {
     // Triggered when royalties are paid out to the artist
     event RoyaltyPaid(address indexed artist, uint256 amount);
 
+    // The modifier restricts function access to users who own at least one NFT
     modifier onlyMintedUser(address user) {
-        // Implement your code here
+        require(YOUR_CODE_GOES_HERE > 0,"Don't own the NFT"); // ASSIGNMENT #2
         _;
     }
 
-    constructor(string memory _name, string memory _symbol, uint256 _nftPrice, string memory _audioURI, address _artist, string memory _coverURI) ERC721(_name, _symbol) {
+    // The constructor will initialize the state variables using the input parameters
+    constructor(string memory _name, string memory _symbol, uint256 _nftPrice, 
+    string memory _audioURI, address _artist, string memory _coverURI) ERC721(_name, _symbol) {
         // Initialize the state variables here
+        _currentTokenId = 0;
+        nftPrice = _nftPrice;
+        // YOUR_CODE_GOES_HERE | ASSIGNMENT #3
     }
 
+    // Mint the new NFT
     function mintNFT(address _to) external payable returns (uint256) {
 		    // Ensures the payment is sufficient.
         require(msg.value >= nftPrice, "Insufficient payment");
 
 		// Increment the token ID and save it to newTokenId here
-        // Your code goes here
+        // YOUR_CODE_GOES_HERE | ASSIGNMENT #4
 
 		// Calculate the royalty amount
         uint256 royaltyAmount = msg.value.mul(ROYALTY_PERCENTAGE).div(100);
@@ -67,12 +80,14 @@ contract SongNFT is ERC721URIStorage, Ownable {
         _setTokenURI(newTokenId, audioURI);
 
 		// Emit the suitable events here
-        // Your code goes here
+        emit NFTMinted(newTokenId, _to, msg.value);
+        emit YOUR_CODE_GOES_HERE // ASSIGNMENT #5
 
 		//  Returns the new token ID
         return newTokenId;
     }
 
+    // payRoyalties function pays out the accumulated royalties to the artist
     function payRoyalties() external {
 		// Retrieves the royalty balance
         uint256 amount = royaltyBalance;
@@ -88,11 +103,14 @@ contract SongNFT is ERC721URIStorage, Ownable {
         emit RoyaltyPaid(artist, amount);
     }
 
+    // Retrieves comprehensive information about the NFT
     function getInfo(address user) external view onlyMintedUser(user) returns (NFTInfo memory)  {
 		// Returns an NFTInfo struct with detailed information.
         return NFTInfo({
-            // Initialize the NFTInfo fields here using state variables
-            // Your code goes here
+            // Initializing the NFTInfo fields here using state variables
+            nftPrice: nftPrice,
+            artist: artist,   
+            // YOUR_CODE_GOES_HERE | ASSIGNMENT #6
         });
     }
 }
--- contracts/paymasters/GeneralPaymaster.sol
+++ contracts/paymasters/GeneralPaymaster.sol
@@ -1,1 +1,115 @@
-// contract code goes here
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// Imports interfaces and constants related to the Paymaster functionality from zkSync contracts.
+import {IPaymaster, ExecutionResult, PAYMASTER_VALIDATION_SUCCESS_MAGIC} 
+from "@matterlabs/zksync-contracts/l2/system-contracts/interfaces/IPaymaster.sol";
+
+// Imports Paymaster flow interface.
+import {IPaymasterFlow} from "@matterlabs/zksync-contracts/l2/system-contracts/interfaces/IPaymasterFlow.sol";
+
+// Imports transaction helper functions and transaction structure from zkSync contracts.
+import {TransactionHelper, Transaction} from "@matterlabs/zksync-contracts/l2/system-contracts/libraries/TransactionHelper.sol";
+
+// Imports various constants used in zkSync.
+import "@matterlabs/zksync-contracts/l2/system-contracts/Constants.sol";
+
+// Imports Ownable contract from OpenZeppelin, which provides basic authorization control functions.
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+// Notice This contract does not include any validations 
+// other than using the paymaster general flow
+
+contract GeneralPaymaster is IPaymaster, Ownable {
+	
+    // Modifier that restricts access to only the bootloader
+    modifier onlyBootloader() {
+		// Ensure the function is called by the bootloader
+        require(
+            // ___________ == BOOTLOADER_FORMAL_ADDRESS, // UNCOMMENT_THIS_LINE_AND_FILL_IN_THE_BLANK
+            "Only bootloader can call this method"
+        );
+        
+        // Continue execution if the condition is met
+        _;
+    }
+
+    // Parameters include two 32-byte values (unused) and a transaction.
+    function validateAndPayForPaymasterTransaction(
+        bytes32,
+        bytes32,
+        Transaction calldata _transaction
+    )
+        external
+        payable
+        onlyBootloader
+        returns (bytes4 magic, bytes memory context)
+    {
+        // By default we consider the transaction as accepted
+        
+        // Set the magic value to indicate a successful validation
+        magic = PAYMASTER_VALIDATION_SUCCESS_MAGIC;
+        
+        // Ensure the paymaster input is at least 4 bytes long.
+        require(
+            _transaction.paymasterInput.length >= ______, // FILL_IN_THE_BLANK
+            "The standard paymaster input must be at least 4 bytes long"
+        );
+        
+		// Extract the first 4 bytes from the paymaster input.
+        bytes4 paymasterInputSelector = bytes4(
+            _transaction.paymasterInput[0:4]
+        );
+        
+        // Check if the selector matches the general paymaster flow.
+        if (paymasterInputSelector == IPaymasterFlow.general.selector) {
+        
+            // Note, that while the minimal amount of ETH needed is tx.gasPrice * tx.gasLimit,
+            // Neither the paymaster nor the account is allowed to access this context variable.
+            
+            // Calculate the required ETH for the transaction.
+            uint256 requiredETH = _transaction.gasLimit * _transaction.maxFeePerGas;
+
+            // The bootloader never returns any data, so it can safely be ignored here.
+            // Transfer the required ETH to the bootloader
+            (bool success, ) = payable(BOOTLOADER_FORMAL_ADDRESS).call{
+                value: requiredETH
+            }("");
+            // Ensure the transfer was successful.
+            require(
+                success,
+                "Failed to transfer tx fee to the Bootloader. Paymaster balance might not be enough."
+            );
+        // Reverts if the paymaster flow is unsupported.
+        } else {
+            revert("Unsupported paymaster flow in paymasterParams.");
+        }
+    }
+
+    // Parameters include context, transaction, two 32-byte values (unused), transaction result, and max refunded gas	
+    function postTransaction(
+        bytes calldata _context,
+        Transaction calldata _transaction,
+        bytes32,
+        bytes32,
+        ExecutionResult _txResult,
+        uint256 _maxRefundedGas
+    // External function, payable, overrides a function from the inherited interface, restricted to bootloader 
+    // This is an empty function body (no additional logic needs to be implemented).   
+    ) external payable override onlyBootloader {}
+
+    // Withdraw funds, restricted to the contract owner
+    function withdraw(address payable _to) external onlyOwner {
+		// Get the contract's current balance
+        // HINT: address() can help you get the address of the contract
+        uint256 balance = _________________; // FILL_IN_THE_BLANK
+        
+        // Transfer the balance to the specified address
+        (bool success, ) = _to.call{value: balance}("");
+        
+        // Ensure the transfer was successful
+        require(__________, "Failed to withdraw funds from paymaster."); // FILL_IN_THE_BLANK
+    }
+
+    receive() external payable {}
+}
