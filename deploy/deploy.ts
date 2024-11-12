68715c11855eedeb3e0e61d4e50ce798f4a875ee
therock ani <anitherock44@gmail.com>
2024-11-12 12:08:12 +0100

    Done with the implementation of zkTune.sol

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,1 +1,11 @@
-// deployment code goes here
+import { deployContract } from "./utils";
+
+export default async function () {
+  const Paymaster = "GeneralPaymaster";
+  const PaymasterArguments = [];
+  await deployContract(Paymaster, PaymasterArguments);
+
+  const contractArtifactName = "zkTune";
+  const dAppArguments = [];
+  await deployContract(contractArtifactName, dAppArguments);
+}
